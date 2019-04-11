64741adfcb6f6d63de6506376068989809c654c4
Daniel Sullivan <sullydm@gmail.com>
2019-04-11 16:31:24 -0400

    2010 block group demogs from mon-coverage

--- census_data/clean/demographics/bg.py
+++ census_data/clean/demographics/bg.py
@@ -5,6 +5,132 @@ from econtools import load_or_build, stata_merge
 from census_data.util.env import data_path, src_path
 
 
+# Block Group Level Cross Sectional Data
+@load_or_build(data_path('bg_demogs_2010.pkl'))
+def load_blockgroup() -> pd.DataFrame:
+    bg_nhgis_file = src_path('demographics',
+                             'nhgis0014_ds176_20105_2010_blck_grp.csv')
+    df = pd.read_csv(bg_nhgis_file, encoding='latin1')
+
+    new_names = _rename_dict_bg()
+    df = df[list(new_names.keys())]         # Restrict
+    df = df.rename(columns=new_names)       # Rename
+    df = df.rename(columns=lambda x: x.lower().replace(' ', '_'))
+
+    id_parts = ['state', 'county', 'tract', 'block_group']
+    df['bg'] = (df[id_parts[0]].astype(str).str.zfill(2)
+                + df[id_parts[1]].astype(str).str.zfill(3)
+                + df[id_parts[2]].astype(str).str.zfill(6)
+                + df[id_parts[3]].astype(str))
+    df = df.drop(id_parts, axis=1)
+    df = df.set_index('bg')
+
+    return df
+
+def _rename_dict_bg():
+    new_names = {
+        # Context Fields
+        'STATEA': 'state',
+        'COUNTYA': 'county',
+        'TRACTA': 'tract',
+        'BLKGRPA': 'block_group',
+        'JMAE001': 'population',
+        # Race
+        # Universe: Total population
+        # Source code: B02001
+        # NHGIS code: JMB
+        'JMBE002': 'race_white',
+        'JMBE003': 'race_black',
+        'JMBE004': 'race_native',
+        'JMBE005': 'race_asian',
+        'JMBE006': 'race_island',
+        'JMBE007': 'race_other',
+        # 'JMBE008': 'Two or more races pop',
+        'JMBE008': 'race_two_or_more',
+        # 'JMBE009': 'Two or more races including other race',
+        'JMBE009': 'race_two_or_more_inc',
+        # 'JMBE010': 'Two or more races excluding other race and three or
+        # more races',
+        'JMBE010': 'race_two_or_more_exc',
+        # Household Type (Including Living Alone)
+        # Universe: Households
+        # Source code: B11001
+        # NHGIS code: JM5
+        'JM5E001': 'households',
+        # Sex by Educational Attainment (Population 25 Years and Over)
+        # Universe: Population 25 years and over
+        # Source code: B15002
+        # NHGIS code: JN9
+        'JN9E001': 'pop_25_plus',
+        'JN9E002': 'pop_male_25_plus',
+        'JN9E019': 'pop_female_25_plus',
+        'JN9E003': 'educ_male_0',
+        'JN9E004': 'educ_male_4',
+        'JN9E005': 'educ_male_6',
+        'JN9E006': 'educ_male_8',
+        'JN9E007': 'educ_male_9',
+        'JN9E008': 'educ_male_10',
+        'JN9E009': 'educ_male_11',
+        'JN9E010': 'educ_male_12',
+        'JN9E011': 'educ_male_hs',
+        'JN9E012': 'educ_male_coll_1',
+        'JN9E013': 'educ_male_coll_nod',
+        'JN9E014': 'educ_male_aa',
+        'JN9E015': 'educ_male_ba',
+        'JN9E016': 'educ_male_ma',
+        'JN9E017': 'educ_male_jd',
+        'JN9E018': 'educ_male_phd',
+        'JN9E020': 'educ_female_0',
+        'JN9E021': 'educ_female_4',
+        'JN9E022': 'educ_female_6',
+        'JN9E023': 'educ_female_8',
+        'JN9E024': 'educ_female_9',
+        'JN9E025': 'educ_female_10',
+        'JN9E026': 'educ_female_11',
+        'JN9E027': 'educ_female_12',
+        'JN9E028': 'educ_female_hs',
+        'JN9E029': 'educ_female_coll_1',
+        'JN9E030': 'educ_female_coll_nod',
+        'JN9E031': 'educ_female_aa',
+        'JN9E032': 'educ_female_ba',
+        'JN9E033': 'educ_female_ma',
+        'JN9E034': 'educ_female_jd',
+        'JN9E035': 'educ_female_phd',
+        # Household Income in the Past 12 Months (2010 Inf-Adjusted)
+        # Universe: Households
+        # Source code: B19001
+        # NHGIS code: JOH
+        'JOHE001': 'hhinc_total',
+        'JOHE002': 'hhinc_0_10000',
+        'JOHE003': 'hhinc_10000_14999',
+        'JOHE004': 'hhinc_15000_19999',
+        'JOHE005': 'hhinc_20000_24999',
+        'JOHE006': 'hhinc_25000_29999',
+        'JOHE007': 'hhinc_30000_34999',
+        'JOHE008': 'hhinc_35000_39999',
+        'JOHE009': 'hhinc_40000_44999',
+        'JOHE010': 'hhinc_45000_49999',
+        'JOHE011': 'hhinc_50000_59999',
+        'JOHE012': 'hhinc_60000_74999',
+        'JOHE013': 'hhinc_75000_99999',
+        'JOHE014': 'hhinc_100000_124999',
+        'JOHE015': 'hhinc_125000_149999',
+        'JOHE016': 'hhinc_150000_199999',
+        'JOHE017': 'hhinc_200000_plus',
+        # Median Household Income
+        # Universe: Households
+        # Source code: B19013
+        # NHGIS code: JOI
+        'JOIE001': 'hhinc_median',
+        # Per Capita Income
+        # Universe: Total population
+        # Source code: B19301
+        # NHGIS code: JQB
+        'JQBE001': 'income_per_capita'
+    }
+    return new_names
+
+
 @load_or_build(data_path('bg_demogs_{year}.p'))
 def bg_demogs(year: int) -> pd.DataFrame:
 
@@ -34,13 +160,12 @@ def bg_demogs(year: int) -> pd.DataFrame:
                   + data['countya'].astype(str).str.zfill(3)
                   + data['tracta'].astype(str).str.zfill(6)
                   + data['blkgrpa'].astype(str))
-
     data.drop(bg, axis=1, inplace=True)
 
     # Set year
     data['year'] = year
 
-    data.set_index('bg', inplace=True)
+    data = data.set_index('bg')
 
     return data
 
@@ -216,5 +341,6 @@ def _rename_dict(year):
 
 
 if __name__ == '__main__':
-    df = load_bgdata(2000, _rebuild=True)
+    # df = load_bgdata(2000, _rebuild=True)
     # df2 = load_bgdata(2005, _rebuild=True)
+    df = load_blockgroup(_load=False)
