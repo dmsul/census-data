ca8700b8603f8533f6db6afb37a708930868d5ad
Daniel Sullivan <sullydm@gmail.com>
2019-04-12 14:56:16 -0400

    Move cbsa xwalk

--- census_data/clean/xwalks/cbsa_to_fips.py
+++ census_data/clean/xwalks/cbsa_to_fips.py
@@ -5,8 +5,8 @@ import pandas as pd
 from census_data.util.env import src_path
 
 
-# TODO: Replace this. It's incomplete in FIPS.
 def load_fips_cbsa():
+    """ NOTE: does not include all FIPS """
     # Read or download the src
     filepath = src_path('cbsa2fipsxw.csv')
     if not os.path.isfile(filepath):
